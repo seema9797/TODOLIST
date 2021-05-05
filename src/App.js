2eb71d56e3798159c735b3404f8a5777e3de006f
Seema Prasad <seemaprasad966@gmail.com>
2021-05-05 22:36:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+  
+// Bootstrap for react
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import ListGroup from 'react-bootstrap/ListGroup';
+  
+  
+class App extends Component  {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userInput : "",
+      list:[]
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  updateInput(value){
+    this.setState({
+      userInput: value,
+    });
+  }
+ 
+  addItem(){
+    if(this.state.userInput !== '' ){
+      const userInput = {
+         id :  Math.random(),
+        value : this.state.userInput
+      };
+  
+    
+      const list = [...this.state.list];
+      list.push(userInput);
+      this.setState({
+        list,
+        userInput:""
+      });
+    }
+  }
+  
+ 
+  deleteItem(key){
+    const list = [...this.state.list];
+    const updateList = list.filter(item => item.id !== key);
+    this.setState({
+      list:updateList,
+    });
+  
+  }
+  
+  render(){
+    return(<Container>
+  
+          <Row style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  fontSize: '3rem',
+                  fontWeight: 'bolder',
+                }}
+                >TODO LIST
+            </Row>
+  
+           <hr/>
+          <Row>
+          <Col md={{ span: 5, offset: 4 }}>
+  
+          <InputGroup className="mb-3">
+          <FormControl
+            placeholder="add item . . . "
+            size="lg"
+            value = {this.state.userInput}
+            onChange = {item => this.updateInput(item.target.value)}
+            aria-label="add something"
+            aria-describedby="basic-addon2"
+          />
+          <InputGroup.Append>
+            <Button
+              variant="dark"
+              size="lg"
+              onClick = {()=>this.addItem()}
+              >
+              ADD
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
+  
+     </Col>
+   </Row>
+   <Row>
+     <Col md={{ span: 5, offset: 4 }}>
+        <ListGroup>
+          {this.state.list.map(item => {return(
+             <ListGroup.Item variant="dark" action 
+              onClick = { () => this.deleteItem(item.id) }>
+              {item.value}
+            </ListGroup.Item>
+  
+         )})}
+        </ListGroup>
+     </Col>
+   </Row>
+     </Container>
+    );
+  }
 }
-
+  
 export default App;
